633bc138c5582e368262427fec3b05f5fc45a0f0
nyyakko <nyakonyns@gmail.com>
2025-06-27 14:40:09 -0300

    use subprocess.h library for managing xsetwacom process

--- LibWacom/source/Device.cpp
+++ LibWacom/source/Device.cpp
@@ -1,5 +1,7 @@
 #include "Device.hpp"
 
+#include "external/subprocess/subprocess.h"
+
 #include <fmt/format.h>
 #include <liberror/Result.hpp>
 #include <liberror/Try.hpp>
@@ -8,27 +10,51 @@
 #include <functional>
 #include <regex>
 #include <sstream>
+#include <array>
 
 using namespace liberror;
 using namespace libwacom;
 
 namespace xsetwacom {
 
-static Result<std::string> execute(std::string_view command)
+namespace {
+
+std::string read_file_descriptor_output(FILE* descriptor)
 {
-    std::string output {};
-    auto fd = popen(fmt::format("xsetwacom {}", command).data(), "r");
-    if (fd == nullptr)
-        return make_error("File descriptor for xsetwacom command returned as nullptr");
+    std::string output;
 
     while (true)
     {
-        std::array<char, 512> buffer {0};
-        if (fgets(buffer.data(), buffer.size(), fd) == nullptr) break;
+        std::array<char, 512> buffer {};
+        if (fgets(buffer.data(), buffer.size(), descriptor) == nullptr) break;
         output.append(buffer.data());
     }
 
-    fclose(fd);
+    return output;
+}
+
+}
+
+static Result<std::string> execute(std::string_view command)
+{
+    std::array<char const*, 3> commandLine { "xsetwacom", command.data(), nullptr };
+    struct subprocess_s subprocess {};
+    auto result = subprocess_create(commandLine.data(), 0, &subprocess);
+
+    if (result != 0)
+    {
+        auto subprocessStderr = subprocess_stderr(&subprocess);
+        auto output = read_file_descriptor_output(subprocessStderr);
+
+        subprocess_destroy(&subprocess);
+
+        return liberror::make_error(output);
+    }
+
+    auto subprocessStdout = subprocess_stdout(&subprocess);
+    auto output = read_file_descriptor_output(subprocessStdout);
+
+    subprocess_destroy(&subprocess);
 
     return output;
 }
@@ -73,12 +99,6 @@ Result<Pressure> libwacom::get_stylus_pressure_curve(int stylus)
     sstream >> pressure.maxX >> pressure.maxY;
     pressure.maxX /= 100.f;
     pressure.maxY /= 100.f;
-
-    if (sstream.fail())
-    {
-        return liberror::make_error(output);
-    }
-
     return pressure;
 }
 
@@ -91,12 +111,7 @@ Result<void> libwacom::set_stylus_pressure_curve(int stylus, Pressure pressure)
         static_cast<int>(std::round(pressure.maxX * 100.f)),
         static_cast<int>(std::round(pressure.maxY * 100.f))
     );
-    auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
+    TRY(xsetwacom::execute(command));
 
     return {};
 }
@@ -109,23 +124,13 @@ Result<int> libwacom::get_stylus_threshold(int stylus)
     std::stringstream sstream(output);
     sstream >> threshold;
 
-    if (sstream.fail())
-    {
-        return liberror::make_error(output);
-    }
-
     return threshold;
 }
 
 Result<void> libwacom::set_stylus_threshold(int stylus, int threshold)
 {
     auto command = fmt::format("--set {} Threshold {}", stylus, threshold);
-    auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
+    TRY(xsetwacom::execute(command));
 
     return {};
 }
@@ -138,11 +143,6 @@ Result<int> libwacom::get_stylus_cursor_proximity(int stylus)
     std::stringstream sstream(output);
     sstream >> proximity;
 
-    if (sstream.fail())
-    {
-        return liberror::make_error(output);
-    }
-
     return proximity;
 }
 
@@ -150,11 +150,6 @@ Result<void> libwacom::set_stylus_cursor_proximity(int stylus, int proximity)
 {
     auto command = fmt::format("--set {} CursorProximity {}", stylus, proximity);
     auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
 
     return {};
 }
@@ -177,11 +172,6 @@ Result<Area> libwacom::get_stylus_area(int stylus)
     sstream >> area.offsetX >> area.offsetY;
     sstream >> area.width >> area.height;
 
-    if (sstream.fail())
-    {
-        return liberror::make_error(output);
-    }
-
     return area;
 }
 
@@ -196,11 +186,6 @@ Result<void> libwacom::set_stylus_area(int stylus, Area area)
     );
     auto output = TRY(xsetwacom::execute(command));
 
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
-
     return {};
 }
 
@@ -209,11 +194,6 @@ Result<void> libwacom::reset_stylus_area(int stylus)
     auto command = fmt::format("--set {} ResetArea", stylus);
     auto output = TRY(xsetwacom::execute(command));
 
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
-
     return {};
 }
 
@@ -221,11 +201,6 @@ Result<void> libwacom::set_stylus_output_from_display_name(int stylus, std::stri
 {
     auto command = fmt::format("--set {} MapToOutput {}", stylus, displayName);
     auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
 
     return {};
 }
@@ -239,12 +214,7 @@ Result<void> libwacom::set_stylus_output_from_display_area(int stylus, Area area
         static_cast<int>(std::round(area.offsetX)),
         static_cast<int>(std::round(area.offsetY))
     );
-    auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
+    TRY(xsetwacom::execute(command));
 
     return {};
 }
@@ -264,12 +234,7 @@ liberror::Result<void> libwacom::set_stylus_handedness(int stylus, Handedness ha
     }
     }
 
-    auto output = TRY(xsetwacom::execute(command));
-
-    if (!output.empty())
-    {
-        return liberror::make_error(output);
-    }
+    TRY(xsetwacom::execute(command));
 
     return {};
 }
